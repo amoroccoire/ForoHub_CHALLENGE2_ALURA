73509690683936be19361f299b93ae468d336184
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:41:24 -0500

    repository de Usuario

--- src/main/java/com/forohub/forohub/user/repositories/UsuarioRepository.java
+++ src/main/java/com/forohub/forohub/user/repositories/UsuarioRepository.java
@@ -1,2 +1,11 @@
-package com.forohub.forohub.user.repositories;public interface UsuarioRepository {
+package com.forohub.forohub.user.repositories;
+
+import com.forohub.forohub.user.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>  {
+    UserDetails findByCorreo(String correo);
 }
