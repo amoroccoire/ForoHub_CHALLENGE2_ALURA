b446d1834c7dba083da15da2fd8e769ff5ff1074
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:44:35 -0500

    token service

--- src/main/java/com/forohub/forohub/auth/TokenService.java
+++ src/main/java/com/forohub/forohub/auth/TokenService.java
@@ -1,2 +1,60 @@
-package com.forohub.forohub.auth;public class TokenService {
+package com.forohub.forohub.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.forohub.forohub.user.entities.Usuario;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.secret}")
+    private String apiSecret;
+    public String generarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            return JWT.create()
+                    .withIssuer("foroHub")
+                    .withSubject(usuario.getCorreo())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException();
+        }
+    }
+
+    public String getSubject(String token) {
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("foroHub")
+                    // reusable verifier instance
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            // Invalid signature/claims
+        }
+        if(verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+
+        return verifier.getSubject();
+    }
+
+    private Instant generarFechaExpiracion() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-05:00"));
+    }
 }
