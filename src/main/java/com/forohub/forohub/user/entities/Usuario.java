5d7f87fdeb3850ff63e244d76cfbe70f4fa2861e
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:40:47 -0500

    problemas con NotNull, algunas partes fueron eliminadas puesto el DTO ya realiza esa verificacion

--- src/main/java/com/forohub/forohub/user/entities/Usuario.java
+++ src/main/java/com/forohub/forohub/user/entities/Usuario.java
@@ -5,8 +5,11 @@ import com.forohub.forohub.topico.entities.Topico;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
@@ -15,16 +18,54 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotNull
     private String correo;
+
     @NotNull
-    private String constrasena;
+    private String contrasena;
 
     @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Topico> topicos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return correo;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
