a8562aa8d14dbbda53d7b9b33c77cf9e984fed08
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-13 22:26:16 -0500

    repository de Topico

---
+++ src/main/java/com/forohub/forohub/topico/repositories/TopicoRepository.java
@@ -0,0 +1,10 @@
+package com.forohub.forohub.topico.repositories;
+
+
+import com.forohub.forohub.topico.entities.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Integer> {
+}
