8945d5460c4f5cc9c3da740d70a5ca11fea7318c
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:43:53 -0500

    configuracion del error

--- src/main/java/com/forohub/forohub/error/GlobalExceptionHandler.java
+++ src/main/java/com/forohub/forohub/error/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
