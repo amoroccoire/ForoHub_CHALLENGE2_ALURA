0d1618e61713a28f53626ffd084c36b860b8df9f
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:45:35 -0500

    security filter, funciona bien

--- src/main/java/com/forohub/forohub/auth/SecurityFilter.java
+++ src/main/java/com/forohub/forohub/auth/SecurityFilter.java
@@ -1,2 +1,41 @@
-package com.forohub.forohub.auth;public class SecurityFilter {
+package com.forohub.forohub.auth;
+
+import com.forohub.forohub.user.repositories.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");//.replace("Bearer ","");
+
+        if (authHeader != null) {
+            var token = authHeader.replace("Bearer ", "");
+            System.out.println(token);
+            System.out.println(tokenService.getSubject(token));
+            var subject = tokenService.getSubject(token);
+            if (subject != null) {
+                var usuario = usuarioRepository.findByCorreo(subject);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
