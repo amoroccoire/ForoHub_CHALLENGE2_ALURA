f209c107692e1b62c63a1742c92d4ab6e8e76f86
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:44:17 -0500

    Dconfiguracion del error 404

--- src/main/java/com/forohub/forohub/error/ResourceNotFoundException.java
+++ src/main/java/com/forohub/forohub/error/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.forohub.forohub.error;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
+
