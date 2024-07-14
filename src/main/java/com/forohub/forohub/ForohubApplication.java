7d55bc85f283e51a0eb5dcecefd4897cd233f6ed
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:40:06 -0500

    agregando orden de mapeo

--- src/main/java/com/forohub/forohub/ForohubApplication.java
+++ src/main/java/com/forohub/forohub/ForohubApplication.java
@@ -2,8 +2,18 @@ package com.forohub.forohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+		"com.forohub.forohub.user.repositories",
+		"com.forohub.forohub.topico.repositories"
+})
+@EntityScan(basePackages = {
+		"com.forohub.forohub.user.entities",
+		"com.forohub.forohub.topico.entities"
+})
 public class ForohubApplication {
 
 	public static void main(String[] args) {
