de458ccbb6ffc670752c287bf1f31ed0681bbfc6
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-14 02:46:47 -0500

    controlador para /login

--- src/main/java/com/forohub/forohub/auth/AuthenticationController.java
+++ src/main/java/com/forohub/forohub/auth/AuthenticationController.java
@@ -1,2 +1,35 @@
-package com.forohub.forohub.auth;public class AuthenticationController {
+package com.forohub.forohub.auth;
+
+import com.forohub.forohub.user.entities.Usuario;
+import com.forohub.forohub.user.entities.dto.UsuarioDTO;
+import jakarta.validation.Valid;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
+    @PostMapping
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+        System.out.println("LLEGO");
+        System.out.println(usuarioDTO.correo() + " ---------- " + usuarioDTO.contrasena());
+        Authentication token = new UsernamePasswordAuthenticationToken(usuarioDTO.correo(), usuarioDTO.contrasena());
+        System.out.println("TERMINA");
+        var usuarioAutenticado = authenticationManager.authenticate(token);
+        var jwtToken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJwtToken(jwtToken));
+    }
 }
